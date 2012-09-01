048d979632fbc23683aeb0cd25489f146093fbc0
Andrew Monks <a@monks.co>
2012-09-01 02:14:38 -0500

    init

--- homer.rb
+++ homer.rb
@@ -36,7 +36,7 @@ get '/' do
 end
 
 get '/init' do
-	memory = Memory.create(:title=>params[:title])
+	memory = Memory.create(:title=>"The beginning of The Odyssey")
 	recollection = memory.recollections.new
 	recollection.attributes = {:author => "Homer", :body => "Tell me, O Muse, of that ingenious hero who travelled far and wide after he had sacked the famous town of Troy. Many cities did he visit, and many were the nations with whose manners and customs he was acquainted; moreover he suffered much by sea while trying to save his own life and bring his men safely home; but do what he might he could not save his men, for they perished through their own sheer folly in eating the cattle of the Sun-god Hyperion; so the god prevented them from ever reaching home. Tell me, too, about all these things, oh daughter of Jove, from whatsoever source you may know them.", :created_at => Time.now}
 	recollection.save
