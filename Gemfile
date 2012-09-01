3a920e1b6e6c599be2912e55ea5b4a994757ffed
Andrew Monks <a@monks.co>
2012-08-31 22:29:33 -0500

    db

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'data_mapper'
+gem 'haml'
+gem 'dm-sqlite-adapter'
--- homer.rb
+++ homer.rb
@@ -1,9 +1,36 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'data_mapper'
 
 set :haml, :format => :html5
 
+
+DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
+
+class Memory
+	include DataMapper::Resource
+
+	property :id,			Serial
+	property :author,		String
+	property :created_at,	DateTime
+	property :body,			Text
+end
+
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
 get '/' do 
-	"Homer!"
+	@memory = Memory.first
+	haml :index
+end
+
+get '/init' do
+	memory = Memory.create(
+		:author		=> 'Homer',
+		:created_at => Time.now,
+		:body		=> "Tell me, O Muse, of that ingenious hero who travelled far and wide after he had sacked the famous town of Troy. Many cities did he visit, and many were the nations with whose manners and customs he was acquainted; moreover he suffered much by sea while trying to save his own life and bring his men safely home; but do what he might he could not save his men, for they perished through their own sheer folly in eating the cattle of the Sun-god Hyperion; so the god prevented them from ever reaching home. Tell me, too, about all these things, oh daughter of Jove, from whatsoever source you may know them."
+	)
+	redirect '/'
 end
