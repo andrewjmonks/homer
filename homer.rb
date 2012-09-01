1ff4b28e59da5e50d96d61b6df6961be0365dc2f
Andrew Monks <a@monks.co>
2012-08-31 22:32:14 -0500

    db

--- homer.rb
+++ homer.rb
@@ -6,7 +6,7 @@ require 'data_mapper'
 set :haml, :format => :html5
 
 
-DataMapper.setup(:default, (ENV["DATABASE_URL"])
+DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))
 
 class Memory
 	include DataMapper::Resource
